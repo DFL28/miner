96626d328ec0e61d2c3ef2e6b3044766b673419e
DFL28 <91407448+DFL28@users.noreply.github.com>
2023-06-09 07:29:12 +0700

    Create tron.sh

---
+++ tron.sh
@@ -0,0 +1,5 @@
+# tron
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u TLk1RX4YuBNQgR8Knb7ejsXd3jac8KSiUw.dfl -p x --cpu 3
